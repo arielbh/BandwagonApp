c7e5e2029dd3826fe658868bcb4e4ac14635168a
Ariel Ben Horesh <arielbh@gmail.com>
2015-05-16 16:56:27 +0300

    Cleaning up sensor code with introdcuing generic SensorViewModel. Adding all sensors.

---
+++ BandwagonApp.Shared/ViewModels/SensorViewModel.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
+using Microsoft.Band;
+using Microsoft.Band.Sensors;
+using SuiteValue.UI.MVVM;
+
+namespace BandwagonApp.ViewModels
+{
+    public class SensorViewModel : ViewModelBase
+    {}
+    public class SensorViewModel<T> : SensorViewModel where T : IBandSensorReading
+    {
+        protected const string InitialMessage = "Not Collecting";
+        protected const string StartCollecting = "Starting to Collect";
+
+        protected bool IsCollecting { get; set; }
+
+        private bool _hasConnection;
+
+        public bool HasConnection
+        {
+            get { return _hasConnection; }
+            set
+            {
+                if (value != _hasConnection)
+                {
+                    _hasConnection = value;
+                    OnPropertyChanged();
+                    ToggleSensorCommand.Refresh();
+                }
+            }
+        }
+
+        
+
+        private DelegateCommand _toggleSensorCommand;
+
+        public DelegateCommand ToggleSensorCommand
+        {
+            get
+            {
+                return _toggleSensorCommand ?? (_toggleSensorCommand = new DelegateCommand(
+                    () =>
+                    {
+
+                        try
+                        {
+                            if (IsCollecting)
+                            {
+                                StopCollection();
+                                return;
+                            }
+                            StartCollection();
+                        }
+                        catch (Exception e)
+                        {
+                            ErrorMessage = "Error with Sensor: " + Name + ": " + e.Message;
+                        }
+                    },
+                    () => HasConnection));
+            }
+        }
+
+        protected async virtual void StartCollection()
+        {
+            if (CheckIsSupported()) return;
+            var hasPermission = await CheckForPermission();
+            if (hasPermission)
+            {
+                await StartReading();
+            }
+            else
+            {
+                ErrorMessage = "Sorry. User has denied permissions for sensor: " + Name;
+            }
+        }
+
+        protected async virtual Task StartReading()
+        {
+            Result = StartCollecting;
+            BandSensor.ReadingChanged += SensorManager_ReadingChanged;
+            IsCollecting = await BandSensor.StartReadingsAsync();
+        }
+
+        private bool CheckIsSupported()
+        {
+            if (!BandSensor.IsSupported)
+            {
+                ErrorMessage = "Sorry." + Name + " Sensor is not supported on this Band.";
+                return true;
+            }
+            return false;
+        }
+
+        protected async virtual Task<bool> CheckForPermission()
+        {
+            if (BandSensor.GetCurrentUserConsent() != UserConsent.Granted)
+            {
+                if (await BandSensor.RequestUserConsentAsync())
+                {
+                    return BandSensor.GetCurrentUserConsent() == UserConsent.Granted;
+                }       
+                return false;
+            }
+            return true;
+        }
+
+        protected async virtual void StopCollection()
+        {
+            BandSensor.ReadingChanged -= SensorManager_ReadingChanged;
+            IsCollecting = false;
+            await BandSensor.StopReadingsAsync();
+            Result = InitialMessage;
+        }
+
+        protected async void SensorManager_ReadingChanged(object sender, BandSensorReadingEventArgs<T> e)
+        {
+            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                if (FormatResultAction != null)
+                {
+                    Result = FormatResultAction(e.SensorReading) + ". T: " + e.SensorReading.Timestamp;
+                }
+            });
+        }
+
+        private IBandSensor<T> _bandSensor;
+
+        public IBandSensor<T> BandSensor
+        {
+            get { return _bandSensor; }
+            set
+            {
+                if (value != _bandSensor)
+                {
+                    _bandSensor = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        private string _errorMessage;
+
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _result = InitialMessage;
+
+        public string Result
+        {
+            get { return _result; }
+            set
+            {
+                if (value != _result)
+                {
+                    _result = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value != _name)
+                {
+                    _name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private Func<T, string> _formatResultAction;
+
+        public Func<T, string> FormatResultAction
+        {
+            get { return _formatResultAction; }
+            set
+            {
+                if (value != _formatResultAction)
+                {
+                    _formatResultAction = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
--- BandwagonApp.Shared/ViewModels/SensorsViewModel.cs
+++ BandwagonApp.Shared/ViewModels/SensorsViewModel.cs
@@ -16,223 +16,93 @@ namespace BandwagonApp.ViewModels
 
         public SensorsViewModel(IBandClient client)
         {
-            _client = client;                      
-        }
-
-        private DelegateCommand _temperturesCommand;
-
-        public DelegateCommand TemperturesCommand
-        {
-            get
+            _client = client;
+            Sensors = new SensorViewModel[]
             {
-                return _temperturesCommand ?? (_temperturesCommand = new DelegateCommand(async () =>
-                    {
-                        if (_tempertureStarted)
-                        {
-                            _client.SensorManager.SkinTemperature.ReadingChanged -= SkinTemperature_ReadingChanged;
-                            Temperture = 0.0;
-                            _tempertureStarted = false;
-                            await _client.SensorManager.SkinTemperature.StopReadingsAsync();
-                            return;
-                        }
-                        var hasPermission = _client.SensorManager.SkinTemperature.GetCurrentUserConsent() ==
-                                            UserConsent.Granted;
-                        if (!hasPermission)
-                        {
-                            hasPermission = await _client.SensorManager.SkinTemperature.RequestUserConsentAsync();
-                        }
-                        if (hasPermission)
-                        {
-                            _client.SensorManager.SkinTemperature.ReadingChanged += SkinTemperature_ReadingChanged;
-                            try
-                            {
-                                _tempertureStarted = await _client.SensorManager.SkinTemperature.StartReadingsAsync();
-                            }
-                            catch (BandException)
-                            {
-                            }
-                        }
-                    }));
-            }
-        }
-
-        private async void SkinTemperature_ReadingChanged(object sender, BandSensorReadingEventArgs<IBandSkinTemperatureReading> e)
-        {
-            CoreDispatcher dispatcher = Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher;
-
-            await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-            {
-                Temperture = e.SensorReading.Temperature;
-            });
-        }
-
-        private double _temperture;
-
-        public double Temperture
-        {
-            get { return _temperture; }
-            set
-            {
-                if (value != _temperture)
+                new SensorViewModel<IBandHeartRateReading>
                 {
-                    _temperture = value;
-                    OnPropertyChanged();
+                    BandSensor = _client.SensorManager.HeartRate,
+                    Name = "Heart Rate",
+                    FormatResultAction = r => "Heart Rate: " + r.HeartRate + " Quality: " + r.Quality,
+                    HasConnection = true,
+                },
+                new SensorViewModel<IBandCaloriesReading>()
+                {
+                    BandSensor = _client.SensorManager.Calories,
+                    Name = "Calories",
+                    FormatResultAction = r => "Calories: " + r.Calories,
+                    HasConnection = true,
+                },
+                new SensorViewModel<IBandSkinTemperatureReading>()
+                {
+                    BandSensor = _client.SensorManager.SkinTemperature,
+                    Name = "Skin Temperture",
+                    FormatResultAction = r => "Temperture: " + r.Temperature,
+                    HasConnection = true,
+                },
+                new SensorViewModel<IBandContactReading>()
+                {
+                    BandSensor = _client.SensorManager.Contact,
+                    Name = "Contact",
+                    FormatResultAction = r => "Contact: " + r.State,
+                    HasConnection = true,
+                },
+                new SensorViewModel<IBandDistanceReading>()
+                {
+                    BandSensor = _client.SensorManager.Distance,
+                    Name = "Distance",
+                    FormatResultAction = r => "Distance: " + r.TotalDistance + 
+                                              " Motion: " + r.CurrentMotion + 
+                                              " Pace:" + r.Pace +
+                                              " Speed:" + r.Speed,
+
+                    HasConnection = true,
+                },
+                new SensorViewModel<IBandUVReading>()
+                {
+                    BandSensor = _client.SensorManager.UV,
+                    Name = "UV",
+                    FormatResultAction = r => "Level: " + r.IndexLevel,
+                    HasConnection = true,
+                },
+                new SensorViewModel<IBandPedometerReading>()
+                {
+                    BandSensor = _client.SensorManager.Pedometer,
+                    Name = "Pedometer",
+                    FormatResultAction = r => "Steps: " + r.TotalSteps,
+                    HasConnection = true,
+                },
+                new SensorViewModel<IBandGyroscopeReading>()
+                {
+                    BandSensor = _client.SensorManager.Gyroscope,
+                    Name = "Gyroscope",
+                    FormatResultAction = r => "X: " + r.AngularVelocityX +
+                                              "Y: " + r.AngularVelocityY +
+                                              "Z: " + r.AngularVelocityZ,
+                    HasConnection = true,
+                },
+                new SensorViewModel<IBandAccelerometerReading>()
+                {
+                    BandSensor = _client.SensorManager.Accelerometer,
+                    Name = "Accelerometer",
+                    FormatResultAction = r => "X: " + r.AccelerationX +
+                                              "Y: " + r.AccelerationY +
+                                              "Z: " + r.AccelerationZ,
+                    HasConnection = true,
                 }
-            }
+            };
         }
 
-        private DelegateCommand _caloriesCommand;
+        private SensorViewModel[] _sensors;
 
-        public DelegateCommand CaloriesCommand
+        public SensorViewModel[] Sensors
         {
-            get
-            {
-                return _caloriesCommand ?? (_caloriesCommand = new DelegateCommand(async () =>
-                    {
-                        if (_caloriesStarted)
-                        {
-                            _client.SensorManager.Calories.ReadingChanged -= Calories_ReadingChanged;
-                            Calories = 0;
-                            await _client.SensorManager.Calories.StopReadingsAsync();
-                            _caloriesStarted = false;
-                            return;
-                        }
-                        var hasPermission = _client.SensorManager.Calories.GetCurrentUserConsent() ==
-                                            UserConsent.Granted;
-                        if (!hasPermission)
-                        {
-                            hasPermission = await _client.SensorManager.Calories.RequestUserConsentAsync();
-                        }
-                        if (hasPermission)
-                        {
-                            _client.SensorManager.Calories.ReadingChanged += Calories_ReadingChanged;
-
-                            try
-                            {
-                                _caloriesStarted = await _client.SensorManager.Calories.StartReadingsAsync();
-                            }
-                            catch (BandException e)
-                            {
-                            }
-                        }
-
-                    }));
-            }
-        }
-
-        private async void Calories_ReadingChanged(object sender, BandSensorReadingEventArgs<IBandCaloriesReading> e)
-        {
-            CoreDispatcher dispatcher = Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher;
-
-            await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-            {
-                Calories = e.SensorReading.Calories;
-            });
-        }
-
-        private long _calories;
-
-        public long Calories
-        {
-            get { return _calories; }
+            get { return _sensors; }
             set
             {
-                if (value != _calories)
+                if (value != _sensors)
                 {
-                    _calories = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        private DelegateCommand _heartRateCommand;
-
-        public DelegateCommand HeartRateCommand
-        {
-            get
-            {
-                return _heartRateCommand ?? (_heartRateCommand = new DelegateCommand(
-                    async () =>
-                    {
-                        if (_heartRateStarted)
-                        {
-                            _client.SensorManager.HeartRate.ReadingChanged -= OnHeartRateOnReadingChanged;
-                            HeartRate = 0;
-                            HeartRateQuality = null;
-                            _heartRateStarted = false;
-                            await _client.SensorManager.HeartRate.StopReadingsAsync();
-                            return;
-                        }
-                        var hasPermission = _client.SensorManager.HeartRate.GetCurrentUserConsent() ==
-                                            UserConsent.Granted;
-                        IEnumerable<TimeSpan> supportedHeartBeatReportingIntervals =
-                            _client.SensorManager.HeartRate.SupportedReportingIntervals;
-               
-                        // check current user heart rate consent
-                        if (!hasPermission)
-                        {
-
-                           // user has not consented, request it
-                           hasPermission = await
-                                _client.SensorManager.HeartRate.RequestUserConsentAsync();
-                            
-                        }
-                       if (hasPermission)
-                       {
-                           _client.SensorManager.HeartRate.ReadingChanged += OnHeartRateOnReadingChanged;
-                           try
-                           {
-                                _heartRateStarted = await _client.SensorManager.HeartRate.StartReadingsAsync();
-                           }
-                           catch (BandException e)
-                           {
-                           }
-
-                       }
-                   }));
-            }
-        }
-
-        private async void OnHeartRateOnReadingChanged(object sender, BandSensorReadingEventArgs<IBandHeartRateReading> args)
-        {
-            // do work when the reading changes (i.e. update a UI element)
-            CoreDispatcher dispatcher = Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher;
-
-            await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-            {
-                HeartRate = args.SensorReading.HeartRate;
-                HeartRateQuality = args.SensorReading.Quality;
-            });
-        }
-
-        private HeartRateQuality? _heartRateQuality;
-
-        public HeartRateQuality? HeartRateQuality
-        {
-            get { return _heartRateQuality; }
-            set
-            {
-                if (value != _heartRateQuality)
-                {
-                    _heartRateQuality = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        private int _heartRate;
-        private bool _heartRateStarted;
-        private bool _caloriesStarted;
-        private bool _tempertureStarted;
-
-        public int HeartRate
-        {
-            get { return _heartRate; }
-            set
-            {
-                if (value != _heartRate)
-                {
-                    _heartRate = value;
+                    _sensors = value;
                     OnPropertyChanged();
                 }
             }
